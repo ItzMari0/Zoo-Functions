6aaf3f04143892ae33f75d8761f93521de3403ec
ItzMari0 <msajr@outlook.com>
2022-06-29 17:29:29 -0300

    Requisito 2 resolvido e refatorado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) => data.species.some((specie) => specie.name === animal
+&& specie.residents.every((animalData) => animalData.age >= age));
 
 module.exports = getAnimalsOlderThan;
