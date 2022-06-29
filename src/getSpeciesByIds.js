b07675a01d629e56e4d38363c0e4f94ee690414f
ItzMari0 <msajr@outlook.com>
2022-06-29 16:06:48 -0300

    Refatoração do Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,5 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  const getSpecies = data.species.filter((specie) => ids.includes(specie.id));
-  return getSpecies;
-}
+const getSpeciesByIds = (...ids) => data.species.filter((specie) => ids.includes(specie.id));
 
 module.exports = getSpeciesByIds;
