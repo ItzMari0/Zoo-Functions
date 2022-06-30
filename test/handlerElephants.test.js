85315a33d599a0609a03bbad8dcb36f73ff7977a
ItzMari0 <msajr@outlook.com>
2022-06-30 16:28:20 -0300

    Requisito 6 resolvido  com 95% de cobertura

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,38 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Retorna o número de elefantes ao utilizar count como parâmetro', () => {
+    const actual = handlerElephants('count');
+    const expected = 4;
+    expect(actual).toBe(expected);
+  });
+
+  it('Retorna um array com o nome de todos os elefantes ao utilizar name como parâmetro', () => {
+    const actual = handlerElephants('names');
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('Retorna a média de idade dos elefantes ao utilizar averageAge como parâmetro', () => {
+    const actual = handlerElephants('averageAge');
+    const expected = 10.5;
+    expect(actual).toBeCloseTo(expected);
+  });
+
+  it('Retorna null se o parâmetro não for específico', () => {
+    const actual = handlerElephants('');
+    const expected = null;
+    expect(actual).toBe(expected);
+  });
+
+  it('Retorna uma mensagem de erro se o parâmetro não for uma string', () => {
+    const actual = handlerElephants(!'string', 'elephants');
+    const expected = 'Parâmetro inválido, é necessário uma string';
+    expect(actual).toBe(expected);
+  });
+
+  it('Retorna undefined se não houver parâmetro', () => {
+    const actual = handlerElephants();
+    expect(actual).toBe(undefined);
+  });
+});
