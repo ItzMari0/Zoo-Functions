8f1cb957f65ca1a964c78255831776bcc23e8488
ItzMari0 <msajr@outlook.com>
2022-07-04 14:37:31 -0300

    Requisito 14: retorno da função sem parâmetro

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const locations = ['NE', 'NW', 'SE', 'SW'];
+
+const sortAnimalLocation = () => species.reduce((acc, animal) => {
+  locations.forEach((location) => {
+    if (animal.location === location) {
+      acc[location].push(animal.name);
+    }
+  });
+  return acc;
+}, { NE: [], NW: [], SE: [], SW: [] });
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || !options.includeNames) {
+    return sortAnimalLocation();
+  }
 }
 
+console.log((getAnimalMap()));
 module.exports = getAnimalMap;
